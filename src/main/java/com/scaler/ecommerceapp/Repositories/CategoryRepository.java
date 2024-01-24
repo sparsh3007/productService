102da64d79e9f4626e56480f8539ef3fa3df671f
Sparsh Raj <sparsh.raj30@gmail.com>
2024-01-24 10:34:16 +0530

    Added cardinlaities, Created Repository classes for Category, price and Product. Exprimented by adding data to the DB using JPA

---
+++ src/main/java/com/scaler/ecommerceapp/Repositories/CategoryRepository.java
@@ -0,0 +1,2 @@
+package com.scaler.ecommerceapp.Repositories;public class CategoryRepository {
+}
---
+++ src/main/java/com/scaler/ecommerceapp/Repositories/ProductRepository.java
@@ -0,0 +1,2 @@
+package com.scaler.ecommerceapp.Repositories;public interface ProductRepository {
+}
