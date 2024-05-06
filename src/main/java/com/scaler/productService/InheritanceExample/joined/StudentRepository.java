135f91e8e913bb73752fb8f4d475ca843542f47c
Sparsh Raj <sparsh.raj30@gmail.com>
2024-05-06 17:27:05 +0530

    Removed inheritance examples

--- src/main/java/com/scaler/productService/InheritanceExample/joined/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.scaler.productService.InheritanceExample.joined;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository("joinedStudentRepository")
-public interface StudentRepository extends JpaRepository<Student, Long>{
-
-}
--- src/main/java/com/scaler/productService/InheritanceExample/tableperclass/StudentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.scaler.productService.InheritanceExample.tableperclass;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
